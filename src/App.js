fbdf06e467319c54eb2859697dd507644219286f
Payal Kumari <kumaripayal68755@gmail.com>
2024-03-12 10:22:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
 
+const Cities = [
+  { name: "None", defaultTemp: null, zipcode: 0 },
+  { name: "Delhi", defaultTemp: 20, zipcode: 110001 },
+  { name: "Mumbai", defaultTemp: 26, zipcode: 400001 },
+  { name: "Chennai", defaultTemp: 28, zipcode: 600001 },
+  { name: "Kolkata", defaultTemp: 23, zipcode: 700001 },
+];
 function App() {
+  const citydropRef = useRef(null);
+  const zipcodeRef = useRef(null);
+  const tempRef = useRef(null);
+
+  const handleCityChange = () => {
+    const selectedCity = citydropRef.current.value;
+    const selectedCityData = Cities.find((city) => city.name === selectedCity);
+    tempRef.current = selectedCityData.defaultTemp;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div class="App">
+      <div class="DropdownDiv">
+        <label class="LabelText">Dropdown Box</label>
+        <select
+          class="DropdownMenu"
+          id="cityDropdown"
+          ref={citydropRef}
+          onChange={handleCityChange}
         >
-          Learn React
-        </a>
-      </header>
+          {Cities.map((city) => (
+            <option key={city} value={city}>
+              {city.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div class="InputText">
+        <label class="LabelText">Zipcode Input</label>
+        <input class="InputField" />
+      </div>
+
+      <div class="Functionality">
+        <button class="SearchButton">Search</button>
+        <button class="ResetButton">Reset</button>
+      </div>
+
+      <div class="OutputBox">
+        <div class="Tempdisplay">
+          {tempRef.current !== null && (
+            <div>Temperature: {tempRef.current}°C</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
